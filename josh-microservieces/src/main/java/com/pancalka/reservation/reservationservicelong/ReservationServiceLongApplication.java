c27705f65a84bffeb7eac22fee6e9b9875cc13f5
panCalka <r.j.r.wojcik@gmail.com>
2018-06-30 01:20:06 +0200

    typo test

--- josh-microservieces/src/main/java/com/pancalka/reservation/reservationservicelong/ReservationServiceLongApplication.java
+++ josh-microservieces/src/main/java/com/pancalka/reservation/reservationservicelong/ReservationServiceLongApplication.java
@@ -37,7 +37,7 @@ public class ReservationServiceLongApplication {
 
 @RefreshScope
 @RestController
-class MessageRestController {
+class MessageRestController {lll
     @Value("${message}")
     private String msg;
 
